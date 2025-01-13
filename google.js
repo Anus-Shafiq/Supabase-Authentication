1708a582fdbb1234f850f3198746e9c7e54b5500
Anus Shafiq <sheikhanus925@gmail.com>
2025-01-13 19:39:26 +0500

    Update google.js

--- google.js
+++ google.js
@@ -2,17 +2,10 @@ let btn = document.getElementById("googleBtn");
 
 async function login() {
   try {
-    const { data, error } = awaitsupabase.auth.signInWithOAuth({
+    const { error } = awaitsupabase.auth.signInWithOAuth({
       provider: "google",
     });
     if (error) throw error;
-    if (data) {
-      Swal.fire({
-        title: "Log In",
-        text: "Log In Successfully",
-        icon: "success",
-      });
-    }
   } catch (error) {
     console.log(error);
   } finally {
